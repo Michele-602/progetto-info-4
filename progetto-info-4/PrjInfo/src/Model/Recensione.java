7a7757f4183d19a64f2875bc2407ca650162704e
manghi89 <ludovico.manghi@studenti.iispascal.it>
2026-05-08 10:24:43 +0200

    angiaaa mettimi sto 10 controllozzo sulle stelline
    Updated Recensione class to handle null values for testo and idRecensione. Added validation for stelle setter to ensure values are within the range of 1 to 5.

--- progetto-info-4/PrjInfo/src/Model/Recensione.java
+++ progetto-info-4/PrjInfo/src/Model/Recensione.java
@@ -9,82 +9,80 @@ package Model;
  * @author nardom
  */
 public class Recensione {
-    private int stelle;
+  private int stelle;
     private String testo;
     private String idRecensione;
     private Cliente cliente;
     private Prodotto prodotto;
 
     public Recensione(int stelle, String testo, String idRecensione, Cliente cliente, Prodotto prodotto) {
-        this.stelle = stelle;
-        this.testo = testo;
-        this.idRecensione = idRecensione;
+        setStelle(stelle); 
+        this.testo = (testo != null) ? testo : "Nessun commento";
+        this.idRecensione = (idRecensione != null) ? idRecensione : "000";
         this.cliente = cliente;
         this.prodotto = prodotto;
     }
 
-    public int getStelle() {
+    public int getStelle() { 
         return stelle;
+    }
+    public String getTesto() { 
+        return testo; 
+    }
+    public String getIdRecensione() {
+        return idRecensione; 
+    }
+    public Cliente getCliente() { 
+        return cliente; 
+    }
+    public Prodotto getProdotto() {
+        return prodotto; 
     }
 
     public void setStelle(int stelle) {
-        this.stelle = stelle;
-    }
-
-    public String getTesto() {
-        return testo;
+  
+        if (stelle < 1) {
+            this.stelle = 1;
+        } else if (stelle > 5) {
+            this.stelle = 5;
+        } else {
+            this.stelle = stelle;
+        }
     }
 
     public void setTesto(String testo) {
-        this.testo = testo;
-    }
-
-    public String getIdRecensione() {
-        return idRecensione;
+        this.testo = (testo != null && !testo.isEmpty()) ? testo : "Nessun commento";
     }
 
     public void setIdRecensione(String idRecensione) {
         this.idRecensione = idRecensione;
     }
 
-    public Cliente getCliente() {
-        return cliente;
-    }
-
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
-    }
-
-    public Prodotto getProdotto() {
-        return prodotto;
     }
 
     public void setProdotto(Prodotto prodotto) {
         this.prodotto = prodotto;
     }
 
+    @Override
+    public String toString() {
+        String s = ""; 
 
-
-
-
-@Override
-public String toString() {
-    String s = ""; 
-    for (int i = 0; i < stelle; i++) {
-        s = s + "★";
+        for (int i = 0; i < stelle; i++) {
+            s = s + "★";
+        }
+        for (int i = stelle; i < 5; i++) {
+            s = s + "☆";
+        }
+        
+     
+        return "ID: " + idRecensione + "\n" +
+               "VALUTAZIONE: " + s + "\n" +
+               "CLIENTE: " + (cliente != null ? cliente.getNome() + " " + cliente.getCognome() : "Anonimo") + "\n" +
+               "RECENSIONE: \"" + testo + "\"";
     }
-    for (int i = stelle; i < 5; i++) {
-        s = s + "☆";
-    }
-    
-    // Usiamo \n per mandare a capo le informazioni
-    return "ID: " + idRecensione + "\n" +
-           "VALUTAZIONE: " + s + "\n" +
-           "CLIENTE: " + cliente.getNome() + "\n" +
-           "RECENSIONE: \"" + testo + "\"\n" +
-           "---------------------------"; 
-}
-   
    
    
    
