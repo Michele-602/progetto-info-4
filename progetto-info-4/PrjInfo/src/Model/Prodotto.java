0d97803982defb40dc2b78906ae65b831db76f70
manghi89 <ludovico.manghi@studenti.iispascal.it>
2026-05-08 10:09:52 +0200

    fine prodotto
    fine prod

--- progetto-info-4/PrjInfo/src/Model/Prodotto.java
+++ progetto-info-4/PrjInfo/src/Model/Prodotto.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
  * @author nardom
  */
 public class Prodotto {
-    private String nome;
+        private String nome;
     private int codice;
      private double medVal;
     private ArrayList<Recensione> recensioni;
@@ -56,7 +56,75 @@ public class Prodotto {
     public void setRecensioni(ArrayList<Recensione> recensioni) {
         this.recensioni = recensioni;
     }
-   
+    
+    ArrayList<Recensione> trovaRecensione(int nstelle){
+        ArrayList<Recensione> b = new ArrayList();
+        for(Recensione x: recensioni){
+            if(x.getStelle()==nstelle) b.add(x);
+                
+        }
+        
+           return b;
+    }
+    ArrayList<Recensione> recPos(){
+        ArrayList<Recensione> b = new ArrayList();
+        for(Recensione x: recensioni){
+            if(x.getStelle()<=3) b.add(x);
+                
+        }
+        
+           return b;
+    }
+    ArrayList<Recensione> recNeg(){
+        ArrayList<Recensione> b = new ArrayList();
+        for(Recensione x: recensioni){
+            if(x.getStelle()>=3) b.add(x);
+                
+        }
+        
+           return b;
+    }
+    ArrayList<Recensione> recCliente(Cliente c){
+        ArrayList<Recensione> b = new ArrayList();
+        for(Recensione x: recensioni){
+            if(x.getCliente()==c)  b.add(x);
+                
+        }
+        
+           return b;
+    }
+    Recensione recCOdice(String codice){
+        ArrayList<Recensione> b = new ArrayList();
+        for(Recensione x: recensioni){
+            if(x.getIdRecensione()==codice)   return x;
+           
+        }
+        return null;
+     }   
+
+@Override
+public String toString() {
+    // Intestazione prodotto
+    String output = "=== SCHEDA PRODOTTO ===\n";
+    output += "NOME   : " + nome + "\n";
+    output += "CODICE : " + codice + "\n";
+    output += "MEDIA  : " + medVal + " / 5.0\n";
+    output += "-----------------------\n";
+    output += "ELENCO RECENSIONI:\n\n";
+
+    if (recensioni.isEmpty()) {
+        output += "   (Nessuna recensione disponibile)\n";
+    } else {
+        for (Recensione r : recensioni) {
+            // Qui Java chiama il toString() di Recensione che abbiamo fatto prima
+            output += r.toString() + "\n";
+            output += "---------------------------\n"; // Separatore tra recensioni
+        }
+    }
+
+    output += "=======================";
+    return output;
+}
    
    
 }
