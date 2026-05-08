de56a25872a58e50053f71e94fb892232a434ab3
manghi89 <ludovico.manghi@studenti.iispascal.it>
2026-05-08 10:11:35 +0200

    viva gyathub naaaaamoooooooooo
    angia mettimi sto 10

--- progetto-info-4/PrjInfo/src/Model/Recensione.java
+++ progetto-info-4/PrjInfo/src/Model/Recensione.java
@@ -12,33 +12,78 @@ public class Recensione {
     private int stelle;
     private String testo;
     private String idRecensione;
+    private Cliente cliente;
+    private Prodotto prodotto;
 
-    public Recensione(int stelle, String testo) {
+    public Recensione(int stelle, String testo, String idRecensione, Cliente cliente, Prodotto prodotto) {
         this.stelle = stelle;
         this.testo = testo;
+        this.idRecensione = idRecensione;
+        this.cliente = cliente;
+        this.prodotto = prodotto;
     }
 
     public int getStelle() {
         return stelle;
     }
 
-    public String getTesto() {
-        return testo;
-    }
-
     public void setStelle(int stelle) {
         this.stelle = stelle;
+    }
+
+    public String getTesto() {
+        return testo;
     }
 
     public void setTesto(String testo) {
         this.testo = testo;
     }
 
-    @Override
-    public String toString() {
-        return "Recensione{" + "stelle=" + stelle + ", testo=" + testo + '}';
+    public String getIdRecensione() {
+        return idRecensione;
     }
-   
+
+    public void setIdRecensione(String idRecensione) {
+        this.idRecensione = idRecensione;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Prodotto getProdotto() {
+        return prodotto;
+    }
+
+    public void setProdotto(Prodotto prodotto) {
+        this.prodotto = prodotto;
+    }
+
+
+
+
+
+@Override
+public String toString() {
+    String s = ""; 
+    for (int i = 0; i < stelle; i++) {
+        s = s + "★";
+    }
+    for (int i = stelle; i < 5; i++) {
+        s = s + "☆";
+    }
+    
+    // Usiamo \n per mandare a capo le informazioni
+    return "ID: " + idRecensione + "\n" +
+           "VALUTAZIONE: " + s + "\n" +
+           "CLIENTE: " + cliente.getNome() + "\n" +
+           "RECENSIONE: \"" + testo + "\"\n" +
+           "---------------------------"; 
+}
    
    
    
